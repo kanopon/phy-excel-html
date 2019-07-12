17321dbef767170de7622f90f496bdd24dcf4e34
geek-mihara <mihara@geek.co.jp>
2019-07-12 09:01:17 +0900

    2019/07/12　エクセル読み込み

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+
+** 今日やったこと
+pythonでexcel読み込み
+
+** 次やること
+html 生成
+html 出力
+
+
+**xlrd
+
+- Bookクラス ワークブック全体
+- Sheetクラス 1つのシート
+- Cellクラス　１つのセル
+
+
 [参考]
 https://note.nkmk.me/python-xlrd-xlwt-usage/
 
--- xlrd_example.py
+++ xlrd_example.py
@@ -1,8 +1,34 @@
+# xls, xlsxの読み込みをおこなるライブラリ
 import xlrd
+# 結果を見やすくするもの
 import pprint
 
+# Bookオブジェクト
 wb = xlrd.open_workbook('data/src/sample.xlsx')
-
 print(type(wb))
-
 print(wb.sheet_names())
+
+# sheetクラス
+sheets = wb.sheets()
+print(type(sheets))
+print(type(sheets[0]))
+
+sheet = wb.sheet_by_name('sheet1')
+print(type(sheet))
+
+cell = sheet.cell(1, 2)
+print(cell)
+print(type(cell))
+print(cell.value)
+
+print(sheet.cell_value(1, 2))
+
+col = sheet.col(1)
+
+print(col)
+print(type(col[0]))
+
+col_values = sheet.col_values(1)
+print(col_values)
+print(sheet.row_values(1))
+
